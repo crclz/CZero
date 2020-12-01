6dcac80f157787a70188be08ec022283fe38e530
crclz <crucialize@foxmail.com>
2020-12-02 00:12:28 +0800

    Add EOFToken class. It is necessary. (#)

---
+++ CZero.Lexical/Tokens/EOFToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CZero.Lexical.Tokens
+{
+    public class EOFToken : Token
+    {
+        public EOFToken(SourcePosition position) : base(position)
+        {
+        }
+    }
+}
