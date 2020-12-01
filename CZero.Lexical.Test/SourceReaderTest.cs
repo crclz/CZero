ea7a38a202d7cde74d31155bef45fa7e0a1f4bf0
crclz <crucialize@foxmail.com>
2020-12-01 23:44:42 +0800

    SourceReader Next test passing

--- CZero.Lexical.Test/SourceReaderTest.cs
+++ CZero.Lexical.Test/SourceReaderTest.cs
@@ -40,5 +40,33 @@ namespace CZero.Lexical.Test
 
             Assert.False(reader.TryNext(out char _));
         }
+
+        [Fact]
+        void NextAtStart_return_first()
+        {
+            var reader = new SourceReader(new StringReader("abc"));
+
+            Assert.Equal('a', reader.Next());
+        }
+
+        [Fact]
+        void NextSecond_return_second()
+        {
+            var reader = new SourceReader(new StringReader("abc"));
+            reader.Next();
+
+            Assert.Equal('b', reader.Next());
+        }
+
+        [Fact]
+        void NextAtEnd_return_false()
+        {
+            var reader = new SourceReader(new StringReader("abc"));
+            reader.Next();
+            reader.Next();
+            reader.Next();
+
+            Assert.False(reader.TryNext(out char _));
+        }
     }
 }
