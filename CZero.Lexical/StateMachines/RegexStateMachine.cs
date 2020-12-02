f37f9a1c937ced06e88f990c2b2f447ce846f74e
crclz <crucialize@foxmail.com>
2020-12-02 11:57:28 +0800

    RegexStateMachine ok

---
+++ CZero.Lexical/StateMachines/RegexStateMachine.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CZero.Lexical.StateMachines
+{
+    class RegexStateMachine : IStateMachine
+    {
+        public Regex WorkingRegex { get; }
+
+        public RegexStateMachine(string pattern)
+        {
+            Guard.Against.Null(pattern, nameof(pattern));
+
+            // Should start with pattern
+            pattern = '^' + pattern;
+
+            WorkingRegex = new Regex(pattern);
+        }
+
+        public bool Consume(SourceStringReader reader)
+        {
+            var match = WorkingRegex.Match(reader.SourceCode, reader.Cursor);
+            if (match.Success)
+            {
+                reader.Advance(match.Length);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
