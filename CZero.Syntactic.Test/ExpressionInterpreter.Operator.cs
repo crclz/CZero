686248ec6e56f49f409b1bbc0208fcc675391263
crclz <crucialize@foxmail.com>
2020-12-05 14:46:00 +0800

    Add ExpressionInterpreter

---
+++ CZero.Syntactic.Test/ExpressionInterpreter.Operator.cs
@@ -0,0 +1,172 @@
+﻿using CZero.Syntactic.Ast.Expressions;
+using CZero.Syntactic.Ast.Expressions.OperatorExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CZero.Syntactic.Test
+{
+    static partial class ExpressionInterpreter
+    {
+        public static object Calculate(this StrongFactorAst strongFactor)
+        {
+            return Calculate(strongFactor.SingleExpression);
+        }
+
+        public static object Calculate(this FactorAst factor)
+        {
+            object value = factor.StrongFactor.Calculate();
+
+            foreach (var item in factor.AsTypeList)
+            {
+                switch (item.TypeToken.Value)
+                {
+                    case "int":
+                        value = (long)value;
+                        break;
+                    case "double":
+                        value = (double)value;
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+
+            return value;
+        }
+
+        public static object Calculate(this TermAst term)
+        {
+            var resultObject = term.Calculate();
+
+            if (resultObject is long longResult)
+            {
+                foreach (var (op, factor) in term.OpFactors)
+                {
+                    var factorVal = factor.Calculate();
+                    if (!(factorVal is long longFactorVal))
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    switch (op.Value)
+                    {
+                        case Lexical.Tokens.Operator.Mult:
+                            longResult *= longFactorVal;
+                            break;
+                        case Lexical.Tokens.Operator.Divide:
+                            longResult /= longFactorVal;
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+
+                return longResult;
+            }
+            else if (resultObject is double doubleResult)
+            {
+                foreach (var (op, factor) in term.OpFactors)
+                {
+                    var factorVal = factor.Calculate();
+                    if (!(factorVal is double doubleFactorVal))
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    switch (op.Value)
+                    {
+                        case Lexical.Tokens.Operator.Mult:
+                            doubleResult *= doubleFactorVal;
+                            break;
+                        case Lexical.Tokens.Operator.Divide:
+                            doubleResult /= doubleFactorVal;
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+
+                return doubleResult;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public static object Calculate(this WeakTermAst weak)
+        {
+            var resultObject = weak.Calculate();
+
+            if (resultObject is long longResult)
+            {
+                foreach (var (op, term) in weak.OpTerms)
+                {
+                    var factorVal = term.Calculate();
+                    if (!(factorVal is long longTermVal))
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    switch (op.Value)
+                    {
+                        case Lexical.Tokens.Operator.Plus:
+                            longResult += longTermVal;
+                            break;
+                        case Lexical.Tokens.Operator.Minus:
+                            longResult -= longTermVal;
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+
+                return longResult;
+            }
+            else if (resultObject is double doubleResult)
+            {
+                foreach (var (op, term) in weak.OpTerms)
+                {
+                    var factorVal = term.Calculate();
+                    if (!(factorVal is double doubleFactorVal))
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    switch (op.Value)
+                    {
+                        case Lexical.Tokens.Operator.Mult:
+                            doubleResult *= doubleFactorVal;
+                            break;
+                        case Lexical.Tokens.Operator.Divide:
+                            doubleResult /= doubleFactorVal;
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+
+                return doubleResult;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public static object Calculate(this OperatorExpressionAst operatorExpression)
+        {
+            // weak_term { 比较符 weak_term }
+            var weakVal = operatorExpression.WeakTerm.Calculate();
+
+            if (operatorExpression.OpTerms.Count > 0)
+            {
+                throw new ArgumentException("comparison unsupported");
+            }
+
+            return weakVal;
+        }
+    }
+}
---
+++ CZero.Syntactic.Test/ExpressionInterpreter.cs
@@ -0,0 +1,84 @@
+﻿using CZero.Lexical.Tokens;
+using CZero.Syntactic.Ast.Expressions;
+using CZero.Syntactic.Ast.Expressions.OperatorExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CZero.Syntactic.Test
+{
+    static partial class ExpressionInterpreter
+    {
+        public static object Calculate(this ExpressionAst expression)
+        {
+            switch (expression)
+            {
+                case OperatorExpressionAst operatorExpression:
+                    return operatorExpression.Calculate();
+                case NegateExpressionAst negateExpression:
+                    return negateExpression.Calculate();
+                case AssignExpressionAst assignExpression:
+                    return assignExpression.Calculate();
+                case CallExpressionAst callExpression:
+                    return callExpression.Calculate();
+                case LiteralExpressionAst literalExpression:
+                    return literalExpression.Calculate();
+                case IdentExpressionAst identExpression:
+                    return identExpression.Calculate();
+                case GroupExpressionAst groupExpression:
+                    return groupExpression.Calculate();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static object Calculate(this NegateExpressionAst negateExpression)
+        {
+            var val = negateExpression.Expression.Calculate();
+
+            if (val is long longVal)
+                return -longVal;
+            if (val is double doubleValue)
+                return -doubleValue;
+            else
+                throw new ArgumentException();
+        }
+
+        public static object Calculate(this AssignExpressionAst assignExpression)
+        {
+            return null;
+        }
+
+        public static object Calculate(this CallExpressionAst callExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Calculate(this LiteralExpressionAst literalExpression)
+        {
+            switch (literalExpression.Literal)
+            {
+                case StringLiteralToken sl:
+                    throw new ArgumentException();
+                case UInt64LiteralToken ul:
+                    return (long)ul.Value;
+                case DoubleLiteralToken dl:
+                    return dl.Value;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static object Calculate(this IdentExpressionAst identExpression)
+        {
+            // TODO: use a fake version, like a1 -- 1, b2 -- 2
+            throw new NotImplementedException();
+        }
+
+        public static object Calculate(this GroupExpressionAst groupExpression)
+        {
+            return groupExpression.Expression.Calculate();
+        }
+    }
+}
