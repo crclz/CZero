4c3426b330e764a808f5b406503395cd8f3bf9c1
crclz <crucialize@foxmail.com>
2020-12-05 20:31:54 +0800

    Add statements empty structures

---
+++ CZero.Syntactic/Ast/Statements/BlockStatementAst.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CZero.Syntactic.Ast.Statements
+{
+    public class BlockStatementAst : StatementAst
+    {
+    }
+}
