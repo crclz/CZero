c4b4fbb633a057d435be75eaea3a86730cce5997
crclz <crucialize@foxmail.com>
2020-12-06 16:01:52 +0800

    ProgramAst complete. All shits waiting for integration test.

---
+++ CZero.Syntactic/Ast/ProgramAst.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.GuardClauses;
+using CZero.Syntactic.Ast.Functions;
+using CZero.Syntactic.Ast.Statements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CZero.Syntactic.Ast
+{
+    public class ProgramAst : Ast
+    {
+        // Not using: program -> decl_stmt* function*
+        // using: program -> (decl_stmt | function)
+
+        public IReadOnlyList<Ast> Elements { get; }
+
+        public ProgramAst(IEnumerable<Ast> elements)
+        {
+            Guard.Against.Null(elements, nameof(elements));
+            Guard.Against.NullElement(elements, nameof(elements));
+
+            // (decl_stmt | function)
+            foreach (var e in elements)
+            {
+                if (!(e is DeclarationStatementAst || e is FunctionAst))
+                {
+                    throw new ArgumentException();
+                }
+            }
+
+            Elements = elements.ToList();
+        }
+    }
+}
--- CZero.Syntactic/SyntacticAnalyzer.cs
+++ CZero.Syntactic/SyntacticAnalyzer.cs
@@ -1,4 +1,7 @@
 ﻿using CZero.Lexical.Tokens;
+using CZero.Syntactic.Ast;
+using CZero.Syntactic.Ast.Functions;
+using CZero.Syntactic.Ast.Statements;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +15,41 @@ namespace CZero.Syntactic
         public SyntacticAnalyzer(TokenReader reader)
         {
             _reader = reader;
+        }
+
+        /// <summary>
+        /// Remember to check reader.ReachedEnd.
+        /// If not reach end, input is bad.
+        /// </summary>
+        public ProgramAst AnalyzeProgram()
+        {
+            // using: program -> (decl_stmt | function)
+
+            var oldCursor = _reader._cursor;
+
+            var elements = new List<Ast.Ast>();
+
+            while (true)
+            {
+                var success = TryDeclarationStatement(out DeclarationStatementAst declarationStatement);
+                if (success)
+                {
+                    elements.Add(declarationStatement);
+                    continue;
+                }
+
+                success = TryFunction(out FunctionAst functionAst);
+                if (success)
+                {
+                    elements.Add(functionAst);
+                    continue;
+                }
+
+                break;
+            }
+
+            var programAst = new ProgramAst(elements);
+            return programAst;
         }
     }
 }
