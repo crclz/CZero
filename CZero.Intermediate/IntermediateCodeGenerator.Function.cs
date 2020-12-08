b546a4487a76d3d79d0e51b19c55bf00baa8c140
crclz <crucialize@foxmail.com>
2020-12-08 22:28:50 +0800

    Function ok

--- CZero.Intermediate/IntermediateCodeGenerator.Function.cs
+++ CZero.Intermediate/IntermediateCodeGenerator.Function.cs
@@ -47,35 +47,48 @@ namespace CZero.Intermediate
             var functionSymbol = new FunctionSymbol(functionAst.Name.Value, returnType, paramTypeList);
             SymbolScope.AddSymbol(functionSymbol);
 
-            if (functionAst.HasParams)
+            // register function
+            if (CodeGenerationEnabled)
             {
-                foreach (var parameter in functionAst.FunctionParamList.FunctionParams)
-                {
-                    var paramType = parameter.Type.Value switch
-                    {
-                        "int" => DataType.Long,
-                        "double" => DataType.Double,
-                        _ => throw new SemanticException($"Bad param type: {parameter.Type.Value}")
-                    };
-
-                    var name = parameter.Name.Value;
-                    if (SymbolScope.FindSymbolShallow(name, out Symbol _))
-                        throw new SemanticException($"Exist symbol: {name}");
-
-                    // Add to scope
-                    SymbolScope.AddSymbol(new VariableSymbol(name, false, parameter.IsConstant, paramType));
-                }
+                GlobalBuilder.RegisterFunction(functionSymbol);
             }
 
-            // All check ok
-
             EnterFunctionDefination(functionSymbol);
-            // Scope of function is created by the caller
-            var canReturn = ProcessBlockStatement(functionAst.BodyBlock, suppressNewScopeCreation: true);
+            {
+                // register params
 
-            if (!canReturn && ReturnCheckEnabled && returnType != DataType.Void)
-                throw new SemanticException($"Cannot leave function {functionAst.Name.Value}");
+                if (functionAst.HasParams)
+                {
+                    foreach (var parameter in functionAst.FunctionParamList.FunctionParams)
+                    {
+                        var paramType = parameter.Type.Value switch
+                        {
+                            "int" => DataType.Long,
+                            "double" => DataType.Double,
+                            _ => throw new SemanticException($"Bad param type: {parameter.Type.Value}")
+                        };
 
+                        var name = parameter.Name.Value;
+                        if (SymbolScope.FindSymbolShallow(name, out Symbol _))
+                            throw new SemanticException($"Exist symbol: {name}");
+
+                        // Add to scope
+                        var argSymbol = new VariableSymbol(name, false, parameter.IsConstant, paramType);
+                        SymbolScope.AddSymbol(argSymbol);
+
+                        if (CodeGenerationEnabled)
+                            CurrentFunction.Builder.RegisterArgument(argSymbol);
+                    }
+                }
+
+
+                // Scope of function is created by the caller
+                var canReturn = ProcessBlockStatement(functionAst.BodyBlock, suppressNewScopeCreation: true);
+
+                if (!canReturn && ReturnCheckEnabled && returnType != DataType.Void)
+                    throw new SemanticException($"Cannot leave function {functionAst.Name.Value}");
+
+            }
             LeaveFunctionDefination();
 
             // implecit void returning
