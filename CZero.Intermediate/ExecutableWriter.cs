adad955fb39c19d2f81d1c52502d2bd1d36b8326
crclz <crucialize@foxmail.com>
2020-12-09 12:08:29 +0800

    Instruction reference ok

---
+++ CZero.Intermediate/ExecutableWriter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CZero.Intermediate
+{
+    public class ExecutableWriter
+    {
+        private MemoryStream Stream { get; } = new MemoryStream();
+
+        private Dictionary<string, byte> InstructionReference = new Dictionary<string, byte>();
+
+        public ExecutableWriter()
+        {
+            GenerateInstructionReference();
+        }
+
+        public void WriteByte(byte b)
+        {
+            Stream.WriteByte(b);
+        }
+
+        public void WriteInt(int x)
+        {
+            var bytes = BitConverter.GetBytes(x);
+            Debug.Assert(bytes.Length == 4);
+
+            foreach (var b in bytes.Reverse())
+            {
+                WriteByte(b);
+            }
+        }
+
+        public void WriteLong(long x)
+        {
+            var bytes = BitConverter.GetBytes(x);
+            Debug.Assert(bytes.Length == 8);
+
+            foreach (var b in bytes.Reverse())
+            {
+                WriteByte(b);
+            }
+        }
+
+        public void WriteDouble(double x)
+        {
+            var bytes = BitConverter.GetBytes(x);
+            Debug.Assert(bytes.Length == 8);
+
+            foreach (var b in bytes.Reverse())
+            {
+                WriteByte(b);
+            }
+        }
+
+        public void WriteOpCode(string opcode)
+        {
+            // get opcode byte
+
+            // write byte
+
+
+        }
+
+        public void GenerateInstructionReference()
+        {
+            var lines = Resource1.InstructionTable.Split("\n");
+            foreach (var line in lines)
+            {
+                var match = Regex.Match(line, @"^(.{4})\s(.+?)\s");
+                Debug.Assert(match.Success);
+
+                var byteString = match.Groups[1].Value;
+                var opcode = match.Groups[2].Value;
+
+                Debug.Assert(byteString.StartsWith("0x"));
+                byteString = byteString[2..];// delete 0x
+
+                var byteData = byte.Parse(byteString, System.Globalization.NumberStyles.HexNumber);
+
+                InstructionReference.Add(opcode, byteData);
+            }
+        }
+    }
+}
--- CZero/Program.cs
+++ CZero/Program.cs
@@ -1,4 +1,5 @@
-﻿using CZero.Intermediate.Test.SemanticCheck.Integration;
+﻿using CZero.Intermediate;
+using CZero.Intermediate.Test.SemanticCheck.Integration;
 using CZero.Syntactic.Test.IntegrationTest;
 using System;
 using System.IO;
@@ -10,10 +11,7 @@ namespace CZero
     {
         static void Main(string[] args)
         {
-            var sourceFile = @"C:\Users\chr\Desktop\compiling\CZero\CZero.Intermediate.Test\SemanticCheck\Integration\Samples\1.c0";
-            var sourceCode = File.ReadAllText(sourceFile);
-
-            var ds = SamplesTest.GetDeclarationStatisticsFromSource(sourceCode);
+            var a = new ExecutableWriter();
 
         }
     }
