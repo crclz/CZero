672b0a88bf53fad6721f56b73dba82a0c1dd2f76
crclz <crucialize@foxmail.com>
2020-12-09 11:33:54 +0800

    Start function generation

--- CZero.Intermediate/AssemblyCodeGenerator.cs
+++ CZero.Intermediate/AssemblyCodeGenerator.cs
@@ -14,6 +14,11 @@ namespace CZero.Intermediate
         {
             var codeLines = GenerateGlobalVars(globalBuilder);
 
+            codeLines.Add("");
+            codeLines.Add("");
+            codeLines.Add("__START__");
+            codeLines.AddRange(GenerateStartFunction(globalBuilder));
+
             codeLines.AddRange(GenerateFunctions(globalBuilder));
 
             return codeLines;
@@ -72,7 +77,7 @@ namespace CZero.Intermediate
         {
             var code = new List<string>();
 
-            foreach (var f in globalBuilder.FunctionsView)
+            foreach (var f in globalBuilder.FunctionsView.Where(p => p.Name != "_start"))
             {
                 var paramList = "";
                 foreach (var param in f.ParamTypes)
@@ -160,6 +165,72 @@ namespace CZero.Intermediate
 
                 code.Add(line);
             }
+
+            return code;
+        }
+
+        static string PlainInstructionToString(Instruction instruction)
+        {
+            var line = (string)instruction.Parts[0];
+
+            foreach (var part in instruction.Parts.ToArray()[1..])
+            {
+                if (part is Instruction ins)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    line += " " + getString(part);
+                }
+            }
+
+            return line;
+        }
+
+        public static List<string> GenerateStartFunction(GlobalBuilder globalBuilder)
+        {
+            // 设置全局变量初始值
+            var code = new List<string>();
+
+            foreach (var v in globalBuilder.GlobalVariablesView)
+            {
+                var vb = v.GlobalVariableBuilder;
+
+                if (vb.StringConstantValue != null)
+                {
+                    // string constant is already in the data after compiling
+                    code.Add($"# {v.Name}: (.data) {vb.StringConstantValue}");
+                }
+                else
+                {
+                    if (!vb.HasInitialValue)
+                    {
+                        code.Add($"# {v.Name}: no initial value");
+                    }
+                    else
+                    {
+                        code.Add($"# {v.Name}: initial value setter code:");
+
+                        // load-addr
+                        code.Add($"globa {vb.Id}");
+
+                        // init-expr
+                        code.AddRange(vb.LoadValueInstructions.Select(p => PlainInstructionToString(p)));
+
+                        // store
+                        code.Add("store.64");
+                    }
+                }
+            }
+
+            // call main
+            var mainFunction = globalBuilder.FunctionsView.Single(p => p.Name == "main");
+
+            // Real generator shoul Check if there is main
+
+            code.Add("");
+            code.Add("call " + mainFunction.Builder.Id);
 
             return code;
         }
